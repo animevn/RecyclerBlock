a21e5c3cd9db45809324bb308e3045cd754eabcb
animevn <haanhth21@gmail.com>
2019-10-25 10:14:50 +0700

    create recyclerviewAdapter

---
+++ app/src/main/java/com/haanhgs/recyclerblockdemo/Adapter.java
@@ -0,0 +1,53 @@
+package com.haanhgs.recyclerblockdemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private List<Song> songs;
+
+    public Adapter(List<Song> songs){
+        this.songs = songs;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Song song = songs.get(position);
+        holder.tvID.setText(String.format("%s", position + 1));
+        holder.tvSong.setText(song.getDetail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvID;
+        private TextView tvSong;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvID = itemView.findViewById(R.id.tvID);
+            tvSong = itemView.findViewById(R.id.tvSong);
+        }
+    }
+}
