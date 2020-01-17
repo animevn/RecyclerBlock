ebfe2556f693e64ab52a365fe3af1219f2ac4a9e
animevn <haanhth21@gmail.com>
2020-01-17 09:28:15 +0700

    reset

--- app/src/main/java/com/haanhgs/recyclerblockdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/recyclerblockdemo/MainActivity.java
@@ -1,10 +1,6 @@
 package com.haanhgs.recyclerblockdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar tbrMain = findViewById(R.id.tbrMain);
-        setSupportActionBar(tbrMain);
-
-        RecyclerView rvSong = findViewById(R.id.rvSong);
-        rvSong.setLayoutManager(new LinearLayoutManager(this));
-        rvSong.setItemAnimator(new DefaultItemAnimator());
-        Adapter adapter = new Adapter(CreateSongList.getSongs());
-        rvSong.setAdapter(adapter);
     }
 }
